054d95801d21fe9e096d70cf0a30dcd6d874cffa
MiguelPeralta123 <maikperalta123@gmail.com>
2023-12-11 19:48:15 -0700

    Added React Native course

--- src/data/certificatesData.js
+++ src/data/certificatesData.js
@@ -10,6 +10,15 @@ export const certificatesData = [
     },
     {
         id: 2,
+        title: 'The Complete React Native Course',
+        image: 'images/certificates/react-native-course.jpg',
+        platform: 'Udemy',
+        duration: '6.5 hours',
+        date: 'November 18, 2023',
+        credential_link: 'https://www.udemy.com/certificate/UC-3eb8f294-d6cb-412a-aa2c-4ea10b69b4ca/',
+    },
+    {
+        id: 3,
         title: 'Kotlin App Developer',
         image: 'images/certificates/kotlin-app-developer.jpg',
         platform: 'Udemy',
@@ -18,7 +27,7 @@ export const certificatesData = [
         credential_link: 'https://www.udemy.com/certificate/UC-bd90a36c-8b7b-4f38-9f96-f01294e97c98/',
     },
     {
-        id: 3,
+        id: 4,
         title: 'Tester',
         image: 'images/certificates/tester.png',
         platform: 'Capacitate para el empleo',
@@ -27,7 +36,7 @@ export const certificatesData = [
         credential_link: 'https://capacitateparaelempleo.org/verifica/d77f65ed-bf44-46af-8d59-2ac4212c2185/0b42e28a-f602-4921-a9e7-c843d7465a05',
     },
     {
-        id: 4,
+        id: 5,
         title: 'Postman API Fundamentals',
         image: 'images/certificates/api-fundamentals.png',
         platform: 'Postman',
